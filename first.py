fb69047a2fac094ea5098fc3312bc0f3a0b00b47
naksvesh-max <naksvesh@gmail.com>
2026-04-09 16:51:08 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,21 @@
+grid = []
+for i in range(6):
+    new_rows = []
+    for c in range(6):
+        new_rows.append(0)
+    grid.append(new_rows)
+
+for i in grid:
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
